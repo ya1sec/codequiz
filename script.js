1c5147810949051582101a71e0764f1c15420901
unknown <theankitbagchi@gmail.com>
2020-07-07 18:14:25 -0400

    pseudocode

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-// WHEN I click the start button
+// DEPENDENCIES/DOM ELEMENTS
+
+// INITIAL DATA
+
+// USER INPUT
+
+// TODO: WHEN I click the start button
 
 // THEN a timer starts and I am presented with a question
 
-// WHEN I answer a question
+// TODO: WHEN I answer a question
 
 // THEN I am presented with another question
 
-// WHEN I answer a question incorrectly
+// TODO: WHEN I answer a question incorrectly
 
 // THEN time is subtracted from the clock
 
-// WHEN all questions are answered or the timer reaches 0
+// TODO: WHEN all questions are answered or the timer reaches 0
 
 // THEN the game is over
 
-// WHEN the game is over
+// TODO:  WHEN the game is over
 
 // THEN I can save my initials and score
